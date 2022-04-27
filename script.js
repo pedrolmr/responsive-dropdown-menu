8f272635fac505c30732d0e06f100dec0a8fa204
pedrolmr <moverapedro@gmail.com>
2022-04-27 01:55:05 -0400

    desktop version done

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let dropdown = document.getElementById("dropdown");
 let submenu = document.querySelector(".sub-menu");
 let dropDownIcon = document.querySelector("i");
+let listItem = document.querySelector(".menu li");
 
-dropdown.addEventListener("click", () => {
+// dropdown.addEventListener("click", () => {
+//     if(submenu.style.display === "none" || dropDownIcon.classList.contains("fa-angle-down")){
+//         submenu.style.display = "block"
+//         dropDownIcon.classList.remove("fa-angle-down");
+//         dropDownIcon.classList.add("fa-angle-up");
+//     }else{
+//         submenu.style.display = "none";
+//         dropDownIcon.classList.remove("fa-angle-up");
+//         dropDownIcon.classList.add("fa-angle-down");
+//     }
+// })
+dropdown.addEventListener("click", function(){
     if(submenu.style.display === "none" || dropDownIcon.classList.contains("fa-angle-down")){
         submenu.style.display = "block"
         dropDownIcon.classList.remove("fa-angle-down");
