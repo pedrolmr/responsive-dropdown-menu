675d1882a1f448be3924c04a22c7a3d326a47fc3
pedrolmr <moverapedro@gmail.com>
2022-04-27 02:46:21 -0400

    added burger icon

--- script.js
+++ script.js
@@ -2,18 +2,10 @@ let dropdown = document.getElementById("dropdown");
 let submenu = document.querySelector(".sub-menu");
 let dropDownIcon = document.querySelector("i");
 let listItem = document.querySelector(".menu li");
+let burgerIcon = document.querySelector(".fa-bars")
+let closeIcon = document.querySelector(".fa-xmark");
+let navBar = document.querySelector("nav");
 
-// dropdown.addEventListener("click", () => {
-//     if(submenu.style.display === "none" || dropDownIcon.classList.contains("fa-angle-down")){
-//         submenu.style.display = "block"
-//         dropDownIcon.classList.remove("fa-angle-down");
-//         dropDownIcon.classList.add("fa-angle-up");
-//     }else{
-//         submenu.style.display = "none";
-//         dropDownIcon.classList.remove("fa-angle-up");
-//         dropDownIcon.classList.add("fa-angle-down");
-//     }
-// })
 dropdown.addEventListener("click", function(){
     if(submenu.style.display === "none" || dropDownIcon.classList.contains("fa-angle-down")){
         submenu.style.display = "block"
@@ -24,4 +16,26 @@ dropdown.addEventListener("click", function(){
         dropDownIcon.classList.remove("fa-angle-up");
         dropDownIcon.classList.add("fa-angle-down");
     }
+});
+
+burgerIcon.addEventListener("click", function(){
+    navBar.style.display = "block";
+    burgerIcon.style.display = "none";
+});
+
+closeIcon.addEventListener("click", function(){
+    navBar.style.display = "none";
+    burgerIcon.style.display = "block";
 })
+
+function resizeListener() {
+    if(window.innerWidth >= 768 ){
+        navBar.style.display = "block";
+        burgerIcon.style.display = "none";
+    }else{
+        navBar.style.display = "none";
+        burgerIcon.style.display = "block";
+    }
+}
+
+window.addEventListener("resize", resizeListener);
